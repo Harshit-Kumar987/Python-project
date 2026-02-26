ac5152e283e99b33063740d4bfea42a4ce628647
Harshit Kumar <harshitkumar4568@gmail.com>
2026-02-26 11:40:26 +0530

    Add files via upload

---
+++ difference's datatype.py
@@ -0,0 +1,31 @@
+# set
+collection= {2,1,2,5,"world","world","hello"}                    #its unordered
+print(collection)
+print(len(collection))                                           #its the total number of items
+print(type(collection))
+
+# dictionary
+dict={
+    "name":"Harshit",
+    "age": 19,
+    "cgpa": 98.3,
+    "marks": 98, 
+}
+dict["subject"]= 7
+print(dict)
+print(type(dict))
+
+# srting
+str= "my name is Harshit.\tI am 17 year old "
+print(str)
+print(type(str))
+
+# list
+student =["karan", 85,"delhi"]
+print(student)
+print(type(student))
+
+# tuple
+tup= (57,74,58,8.5,48)
+print(tup)
+print(type(tup))
