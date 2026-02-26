17ff332024a0fac06ddd4fe03c4e8c02d0066ac0
Harshit Kumar <harshitkumar4568@gmail.com>
2026-02-26 11:38:52 +0530

    Add files via upload

---
+++ list methods.py
@@ -0,0 +1,39 @@
+Lists Methods
+
+1.
+list = [3,5,4]
+list.append(2)
+print(list)
+
+2.
+list= [3,5,6,3.5]
+list.sort()
+print(list)
+list= ["zjif", "idwjd" ,"fie"]
+list.sort()
+print(list)
+
+3.
+list= [2,23.5,3,24,7]
+list.sort(reverse=True)
+print(list)
+
+4.
+list= [8,4,7,5,6]
+list.reverse()
+print(list)
+
+5.
+list= [5,3,9,6]
+list.insert(2,4)
+print(list)
+
+6.
+list= [6,7,4,7,5]
+list.remove(7)
+print(list)
+
+7.
+list= [8,6,3,5,8,5]
+list.pop(3)
+print(list)
